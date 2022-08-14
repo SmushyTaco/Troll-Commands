e6442d6f83aa022b14b3cd1fa4814f4b37b95f9b
SmushyTaco <smushytaco@gmail.com>
2022-08-14 03:31:47 -0700

    1.19.2

--- Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/commands/base/CustomSoundInstance.kt
+++ Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/commands/base/CustomSoundInstance.kt
@@ -6,8 +6,9 @@ import net.minecraft.client.sound.MovingSoundInstance
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
+import net.minecraft.util.math.random.Random
 @Environment(EnvType.CLIENT)
-class CustomSoundInstance(sound: SoundEvent?): MovingSoundInstance(sound ?: SoundEvents.BLOCK_SAND_BREAK, SoundCategory.MASTER) {
+class CustomSoundInstance(sound: SoundEvent?): MovingSoundInstance(sound ?: SoundEvents.BLOCK_SAND_BREAK, SoundCategory.MASTER, Random.create()) {
     override fun tick() {
         val player = MinecraftClient.getInstance().player ?: return
         x = player.x
--- Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/commands/base/TrollKickCommand.kt
+++ Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/commands/base/TrollKickCommand.kt
@@ -3,7 +3,7 @@ import kotlinx.coroutines.*
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.sound.SoundEvent
-import net.minecraft.text.TranslatableText
+import net.minecraft.text.Text
 class TrollKickCommand(command: String, condition: BooleanReturn, imagePaths: Array<String>?, sound: SoundEvent? = null): TrollCommand(command, condition, imagePaths, sound) {
     private val disconnectJob
         get() = CoroutineScope(Dispatchers.Default).launch {
@@ -11,7 +11,7 @@ class TrollKickCommand(command: String, condition: BooleanReturn, imagePaths: Ar
             delay(1000)
             if (!isBeingTrolled || !condition() || MinecraftClient.getInstance().player == null) return@launch
             isBeingTrolled = false
-            MinecraftClient.getInstance().networkHandler?.connection?.disconnect(TranslatableText("multiplayer.disconnect.flying"))
+            MinecraftClient.getInstance().networkHandler?.connection?.disconnect(Text.translatable("multiplayer.disconnect.flying"))
         }
     private lateinit var cachedDisconnectJob: Job
     override fun command(matrixStack: MatrixStack) {
