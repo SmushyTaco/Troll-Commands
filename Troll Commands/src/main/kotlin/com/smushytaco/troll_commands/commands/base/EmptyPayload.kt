797e9429a3db34e99a901e3f74e5865c9bff9706
Nikan Radan <personal@nikanradan.com>
2024-07-08 20:09:13 -0700

    Bug Fix

--- Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/commands/base/EmptyPayload.kt
+++ Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/commands/base/EmptyPayload.kt
@@ -1,9 +1,10 @@
 package com.smushytaco.troll_commands.commands.base
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry
 import net.minecraft.network.codec.PacketCodec
+import net.minecraft.network.codec.PacketCodecs
 import net.minecraft.network.packet.CustomPayload
 import net.minecraft.util.Identifier
 class EmptyPayload(private val identifier: Identifier) : CustomPayload {
     override fun getId(): CustomPayload.Id<EmptyPayload> = CustomPayload.Id(identifier)
-    fun register() { PayloadTypeRegistry.playS2C().register(id, PacketCodec.unit(this)) }
+    fun register() { PayloadTypeRegistry.playS2C().register(id, PacketCodec.tuple(PacketCodecs.STRING, { it.identifier.toString() }) { EmptyPayload(Identifier.of(it)) }) }
 }
