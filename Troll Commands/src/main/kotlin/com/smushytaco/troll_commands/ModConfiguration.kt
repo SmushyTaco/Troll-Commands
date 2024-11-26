0a91dc806ad7410b2295668a9feaae6de894b2e1
Nikan Radan <personal@nikanradan.com>
2024-11-25 17:25:52 -0800

    New Config

---
+++ Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/ModConfiguration.kt
@@ -0,0 +1,21 @@
+package com.smushytaco.troll_commands
+import io.wispforest.owo.config.annotation.*
+@Modmenu(modId = TrollCommands.MOD_ID)
+@Config(name = TrollCommands.MOD_ID, wrapperName = "ModConfig")
+@Suppress("UNUSED")
+class ModConfiguration {
+    @JvmField
+    var canBeJumpscared = true
+    @JvmField
+    var canBeHotDogged = true
+    @JvmField
+    var canBeRickRolled = true
+    @JvmField
+    var canBeAmongUsed = true
+    @JvmField
+    var canBePumpkined = true
+    @JvmField
+    var canBeReplayed = true
+    @JvmField
+    var canBeCrashed = true
+}
