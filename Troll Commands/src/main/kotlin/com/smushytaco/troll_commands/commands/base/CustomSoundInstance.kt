33a0931866a3c9fc1ef6d9e59bf09e15a8819d69
Nikan Radan <github.com@alias.nikanradan.com>
2024-12-13 00:35:28 -0800

    1.21.4

--- Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/commands/base/CustomSoundInstance.kt
+++ Troll Commands/src/main/kotlin/com/smushytaco/troll_commands/commands/base/CustomSoundInstance.kt
@@ -1,13 +1,10 @@
 package com.smushytaco.troll_commands.commands.base
-import net.fabricmc.api.EnvType
-import net.fabricmc.api.Environment
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.sound.MovingSoundInstance
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
 import net.minecraft.util.math.random.Random
-@Environment(EnvType.CLIENT)
 class CustomSoundInstance(sound: SoundEvent?): MovingSoundInstance(sound ?: SoundEvents.BLOCK_SAND_BREAK, SoundCategory.MASTER, Random.create()) {
     override fun tick() {
         val player = MinecraftClient.getInstance().player ?: return
